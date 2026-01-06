ef9320afa7755b837e149f53f18518d82629c0e3
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2026-01-06 13:18:16 +0400

    add 2 float arguments and return float

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 """
-    Adds two float numbers and returns their sum
+Module that provides a function to add two numbers.
 """
 
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum.
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float: the sum of a and b
+    """
     return a + b
