a5b7ae8deef26340f851659b9d888f158499f57c
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2026-01-06 13:47:52 +0400

    Floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Function that that akes a float n and returns floor as float"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float n
+    
+    Args:
+    n (float): the number put to floor
+    
+    Returns:
+    floor as value n
+    """
+    return math.floor(n)
