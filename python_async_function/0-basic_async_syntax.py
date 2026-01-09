aaebc9b2d8371fe4f3b3e3b3294d5a7ae936d437
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2026-01-09 19:17:15 +0400

    Basic async

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """waits for a random delay between 0 to max delay
+    and returns it"""
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
