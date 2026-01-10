cbdda0f12657d539de447afa5d0b07cd93d934ad
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2026-01-10 20:37:27 +0400

    Tasks4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,10 +6,12 @@ task_wait_random is being called."""
 
 
 import asyncio
+from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Run task_wait_random n times and return the results
     in ascending order of completion
