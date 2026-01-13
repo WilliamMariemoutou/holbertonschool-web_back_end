c7f354d807f5bb61b3d2ebde4bf969b815cb0334
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2026-01-13 22:35:36 +0400

    Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+def index_range(page, page_size):
+    """Calculates the starting and ending"""
+    
+    start = (page - 1) * page_size
+    
+    end = page * page_size
+    
+    return (start, end)
