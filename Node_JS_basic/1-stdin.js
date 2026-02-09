3e93b1d13b88a0f2d553d5218cb22de52d7c82b1
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2026-02-09 19:53:40 +0400

    STDIN

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
 
-process.stdin.on('data', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.trim()}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
